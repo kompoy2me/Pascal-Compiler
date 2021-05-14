b66d1a4bdf3cc579f5fbbc1613a9adb3dd471415
kompoy2me <boshmachkin@gmail.com>
2021-05-14 10:49:27 +1000

    fixing bugs

--- analyzation1.py
+++ analyzation1.py
@@ -6,6 +6,7 @@ from dict_of_states import dict_of_states
 class analyzation:
 	def __init__(self, Path):
 		self.Separators = [' ', '\n', '\t', '\0', '\r']	
+		self.Delimiters = ['+=', '-=', '*=', '/=', ':=', '<>', '<=', '>=', '..','.', ';', ',', '(', ')', '+', '-', '*', '/', '=', '>', '<', '[', ']', ':', '}','{']
 		self.Key_words = ['string','integer','shortint','smallint','byte','int64','word','longword','cardinal','uint64','BigInteger',
 		'real','double', 'single', 'decimal','boolean', 'char','False','True','and', 'array', 'begin', 'case', 'const', 'do', 'downto',
 		'else', 'end','file', 'for', 'foreach', 'function', 'goto', 'if', 'in', 'label', 'mod', 'nil','not', 'of', 'or', 'packed', 
@@ -13,8 +14,12 @@ class analyzation:
 		self.curr_symbs = ['','']
 		self.input_file = open(Path, 'r')
 		self.error_state = False
+		self.init_array = False
+		self.end_lexem = False
 		#ТЕКУЩИЕ СИМВОЛ И ПОЗИЦИЯ
-		self.curr_p = -1
+		self.curr_p = 0
+		self.curr_l = 1
+		self.lexem_pos = '1:1'
 		self.curr_s = ""
 		
 		
@@ -34,44 +39,73 @@ class analyzation:
 		buff = Buffer()
 		stat = State()
 		dict_state = dict_of_states()
-
+		lexem_type = "_"
+		
 		if  self.error_state:
 			self.error_state  = False
-			return Lexer(self.curr_p - len(buff.get_buff()), 'eof', '', '')
+			return self.create_lex(buff, 3)
 
 		if self.curr_symbs[1] == '':
 			self.curr_symbs[1] = self.get_next_symb()
 
 		while True:
 			self.get__next()
-
-			if stat.get_state() == 1:
-				next_state = dict_state.find_state( stat.get_state(), self.define_symb(self.curr_symbs[0]), self.define_symb(self.curr_symbs[0]))
-				stat.set_state( next_state  )
 			
-			#print( self.curr_p,'  ', self.curr_symbs[0], self.define_symb(self.curr_symbs[0]) )
+			if stat.get_state() == 1:
+				next_state = dict_state.find_state( stat.get_state(), self.define_symb(self.curr_symbs[1]), self.define_symb(self.curr_symbs[0]))
+				stat.set_state( next_state  )
+
+			lexem_type = stat.get_state()
 			next_state = dict_state.find_state( stat.get_state(), self.define_symb(self.curr_symbs[0]), self.define_symb(self.curr_symbs[1]))
+			#print( stat.get_state(), '--->' ,next_state)
+
 			self.state_action(stat.get_state(),buff)
-			#print('  ', stat.get_state(), '->' ,next_state)
 			
 			if stat.get_state() == 7:
 				self.error_state = True
 				lex_len = len(buff.get_buff())-1
 				if self.curr_symbs[0] == "":
 					lex_len = len(buff.get_buff())
-				return Lexer(self.curr_p - lex_len, 'error', buff.get_buff(), buff.get_buff())
+				return self.create_lex(buff, 7)	
+
+			if stat.get_state() == 4 and self.define_symb(self.curr_symbs[0]) == "N" and self.curr_symbs[1] == ".":
+				self.get__next()
+				if self.define_symb(self.curr_symbs[1]) == "N":
+					next_state = 41
+					buff.add_buff(self.curr_symbs[0])
+				elif self.define_symb(self.curr_symbs[1]) == ".":
+					self.init_array = True
+					return self.create_lex(buff, 4)
+				else:
+					next_state = 7
+					buff.add_buff(self.curr_symbs[0])
+
 
 			stat.set_state( next_state )
+
 			
+			#print(lexem_type, self.curr_symbs[0], self.lexem_pos)
 			if  stat.get_state() == 1:
+				if lexem_type == 80:
+					buff.clear_buff()
+					self.lexem_pos = -1
 				if buff.get_buff() != "":
-					return self.create_lex(buff, dict_state.find_state( stat.get_state(), self.define_symb(self.curr_symbs[0]), self.define_symb(self.curr_symbs[0])))
+					if lexem_type == 9:
+						if buff.get_buff()[0] == buff.get_buff()[-1] == "'":
+							return self.create_lex(buff, 5)
+						if not buff.get_buff() in self.Delimiters:
+							stat.set_state(7)
+						else:
+							return self.create_lex(buff, lexem_type)
+					else: 
+						return self.create_lex(buff, lexem_type)
 
 			if stat.get_state() == 3:
 				if buff.get_buff() == "":
-					return Lexer(self.curr_p - len(buff.get_buff()), 'eof', '', '')
+					return self.create_lex(buff, 3)
 				else:
 					return self.create_lex(buff, dict_state.find_state( 1, self.define_symb(self.curr_symbs[0]), self.define_symb(self.curr_symbs[0])))
+
 #
 #		MAIN
 #
@@ -83,17 +117,30 @@ class analyzation:
 		else:
 			return ""
 
-
-
 	def get__next(self):
-		self.curr_p += 1
 		self.curr_symbs[0] = self.curr_symbs[1]
 		self.curr_symbs[1] = self.get_next_symb()
-
+		if self.curr_symbs[0] == '\n':
+			 self.curr_l += 1
+			 self.curr_p = -1
+		self.curr_p += 1
+		
 
 	def state_action(self, stat, buff):
-		if stat == 4 or stat == 5 or stat == 6 or stat == 9 or stat == 41 or stat == 3 or stat == 7:
+		
+		if stat != 1 and stat != 81 and stat != 80:
+			if self.lexem_pos == -1:
+				self.lexem_pos = str(self.curr_l) + ':' + str(self.curr_p)
+			if  stat == 9 and buff.get_buff() == "/" :
+				print(self.curr_symbs[0], self.curr_symbs[1] )
+
 			buff.add_buff(self.curr_symbs[0])
+			if self.init_array:
+				#if buff.get_buff()==".":
+				buff.add_buff(self.curr_symbs[0])
+				self.init_array = False
+		
+			
 
 
 	def define_symb(self,symb):
@@ -102,55 +149,57 @@ class analyzation:
 		elif symb.isalpha():
 			if symb == "e":
 				return "E"
+			elif symb in ['A','B','C','D','E','F']:
+				return "D"
 			else:
 				return "L"
 		elif symb.isdigit():
 			return "N"
 		elif symb in self.Separators:
+			if symb == "\n":
+				return "\n"
 			return "S"
 		elif symb == "+" or symb == "*":
 			return "I"
 		elif symb == ">" or symb == "<":
 			return "W"
-		elif symb == "'" or symb == '"':
-			return "V"
 		else:
 			return symb
 
 
 	def create_lex(self, buff, state):
 		value = buff.get_buff()
+
 		if state == 4:
-			if buff.get_buff().find('.') != -1:
-				type_s = 'float'
-				value = float(buff.get_buff())
-			else:
-				type_s = 'number'	
-			'''lex_str = buff.get_buff()
-			i = buff.get_buff().find('..')
-			if i != (-1):
-				with open("result.txt", "a") as f:
-					f.write(str(self.curr_p - len(buff.get_buff())+1) + "	" + type_s + "	" + lex_str[0:i] +'\n')
-					f.write(str(self.curr_p - len(buff.get_buff())+1+i) + "	" + 'delimiter' + "	" + lex_str[i:i+2] +'\n')
-				buff.clear_buff()
-				buff.add_buff(lex_str[i+2:])'''
+			type_s = 'int'
+
 		elif state == 5:
 			type_s = 'string'
+			value = value.replace("'",'')
 		elif state == 6:
-			if buff.get_buff() in self.Key_words:
+			if value in self.Key_words:
 				type_s = 'reserved id'
 			else:
-				type_s = 'id'
+				type_s = 'id'				
 		elif state == 9:
 			type_s = 'delimiter'
-		elif state == 8:
-			type_s = 'comment'
+
+		elif state == 41:
+			type_s = 'float'
+			value = float(buff.get_buff())
+		elif state == 2:
+			type_s = 'hex'
+			value = hex(int(value.replace('$',""), 16))
 		elif state == 7:
 			type_s = 'error'
+		elif state == 3:
+			type_s = 'eof'
 		else:
 			type_s = state
-
-		new_lex = Lexer(self.curr_p - len(buff.get_buff())+1, type_s, buff.get_buff(), value)
+		new_lex = Lexer(self.lexem_pos, type_s, buff.get_buff(), value)
+		
+		if state != 7:
+			self.lexem_pos = -1
 		buff.clear_buff()
 		return new_lex
 	
--- test.py
+++ test.py
@@ -4,33 +4,38 @@ class test:
 		self.check()
 
 	def check(self):
-
-		for i in range(1,10):
+		count = 0
+		for i in range(1,23):
 			print("_____________TEST №", i,"_____________")
 			test_prog = "tests\\test"+str(i)+".txt"
 			
 			analyz = analyzation(test_prog)
+			
 			while True:
 				lexem = analyz.analyzer()
 				with open("result.txt", "a") as f:
-					f.write("%5d%16s%16s%16s" % (lexem.get_pos(), str(lexem.get_type()),str(lexem.get_orig()), str(lexem.get_value()) + '\n'))
+					f.write("%5s%16s%16s%16s" % (lexem.get_pos(), str(lexem.get_type()),str(lexem.get_orig()), str(lexem.get_value()) + '\n'))
 				if  lexem.get_type() == 'eof':
 					break
 
 			with open("result.txt", "r") as f:
 				result = f.read()
-			with open("tests\\res"+str(i)+".txt", "r") as f:
+			with open("tests\\test"+str(i)+"_res.txt", "r") as f:
 				test_result = f.read()
 			
-			#print(result)
+			print(result)
 			if result == test_result:
 				print('OK')
+				count += 1
 			else:
 				print('WRONG')
+				'''for i in range(0,len(test_result)-1):
+					if result[i] != test_result[i]:
+						print('position')
+						print(i, test_result[i], result[i])
+					break'''
 
 
 			with open("result.txt", "w") as f:
 				f = ""
-
-			
-
+		print('Всего пройдено тестов ' + str(count) + '/22')
